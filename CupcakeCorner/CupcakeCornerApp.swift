c3eee62e0dc7dc3175015b0ba211bd46d3a29392
YossiBenZaken <yossibz@gmail.com>
2022-03-21 21:26:20 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Yosef Ben Zaken on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
